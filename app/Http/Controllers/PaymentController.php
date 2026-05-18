068a95c577c09b3261e564bce3622c7355cad67b
Naser Man <tgza54@gmail.com>
2026-05-19 00:36:03 +0300

    Now You can paywith Moyaser  method

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -43,7 +43,7 @@ $response = Http::withBasicAuth(
     'callback_url' => url('https://qatar-life-production.up.railway.app/api/payment/callback?session_id=' . $paymentSessionId),
     'error_url' => 'http://localhost:5173/payment-failed',
   'source' => [
-        'type' => 'creditcard',
+        'type' => 'payment_page',
         'manual' => false,
     ]
 ]);
