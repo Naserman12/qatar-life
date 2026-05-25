930da6512cf00dc76c84108b19665da859c64fcf
Naser Man <tgza54@gmail.com>
2026-05-26 00:50:46 +0300

    Carbon parse Payment ID : succes

--- database/migrations/2025_09_22_162552_create_products_table.php
+++ database/migrations/2025_09_22_162552_create_products_table.php
@@ -23,6 +23,23 @@ return new class extends Migration
         $table->string('category')->nullable();     // تصنيف المنتج
         $table->text('description')->nullable();
          $table->boolean('tax_included')->default(true); // هل السعر شامل الضريبة
+
+         
+                     // نوع المنتج (عادي أو مصحف)
+            $table->string('type')->default('product');
+
+            // بيانات المصحف (اختياري)
+            $table->string('publisher')->nullable();   // دار النشر
+            $table->integer('pages')->nullable();      // عدد الصفحات
+            $table->string('language')->nullable();    // اللغة
+            $table->string('cover_type')->nullable();  // نوع الغلاف
+
+            // لو فيه طبعة أو إصدار
+            $table->string('edition')->nullable();
+
+            // هل هو مصحف فعلي
+            $table->boolean('is_quran')->default(false);
+         
         $table->timestamps();
         });
     }
